7a1d1fc17afc86e07f9fb25bc1ffcc583f99da87
NKIIB nishikiout <48525357+nkiib@users.noreply.github.com>
2022-01-10 14:45:05 +0900

    ラジオボタンを設置

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ import PySimpleGUI as sg
 sg.theme('DarkAmber')   # デザインテーマの設定
 
 # ウィンドウに配置するコンポーネント
+radio_dic = {
+    '-1-' : 'HTML',
+    '-2-' : 'テキストファイル',
+    '-3-' : 'CSV形式',
+    '-4-' : 'XML形式'
+}
+
 layout = [  [sg.Text('RSSからHTMLを出力するジェネレーター')],
             [sg.Text('RSS URL'), sg.InputText()],
             [sg.Text('ファイル名'), sg.InputText()],
+            [sg.Radio(item[1], key=item[0], group_id='0') for item in radio_dic.items()],
             [sg.Button('OK'), sg.Button('キャンセル')] ]
 
 # ウィンドウの生成
-window = sg.Window('サンプルプログラム', layout,font=('Arial',20))
+window = sg.Window('Tlooks RSS to file', layout,font=('Arial',20))
 
 # イベントループ
 while True:
@@ -24,33 +32,36 @@ while True:
         d = feedparser.parse(rssurl)
         outx = ""
 
-        for entry in d['entries']:
-            outx += '<a href="'+ entry.link + '">' + entry.title + "</a><br>"
-            outx += entry.published + "<br><br>"
+        if '-1-' == True:
+            
+            for entry in d['entries']:
+                outx += '<a href="'+ entry.link + '">' + entry.title + "</a><br>"
+                outx += entry.published + "<br><br>"
 
-        def write1( file1, str1 ): 
-            with open( file1, 'w', encoding='utf-8' ) as f1: 
-                f1.write( str1 ) 
-            return 0 
+            def write1( file1, str1 ): 
+                with open( file1, 'w', encoding='utf-8' ) as f1: 
+                    f1.write( str1 ) 
+                return 0 
 
+            
+
+            str1 = '''
+            <html>
+            <head>
+            <meta charset="utf-8">
+            <title>{title1}</title>
+            </head>
+            <body>
+            {body1} 
+            </body>
+            </html>
+            '''.format( title1 = "output", body1 = outx ) 
+
+            #print( str1 ) 
+
+            path1 = os.path.dirname(__file__) + "/" 
+            file1 = path1 + values[1] + ".html"
+            write1( file1, str1 ) 
+            print(values)
         
-
-        str1 = '''
-        <html>
-        <head>
-        <meta charset="utf-8">
-        <title>{title1}</title>
-        </head>
-        <body>
-        {body1} 
-        </body>
-        </html>
-        '''.format( title1 = "output", body1 = outx ) 
-
-        #print( str1 ) 
-
-        path1 = os.path.dirname(__file__) + "/" 
-        file1 = path1 + values[1] + ".html"
-        write1( file1, str1 ) 
-
 window.close()
